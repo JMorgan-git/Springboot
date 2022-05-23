c8f97924d6ccf355967f2958398441164fc3e0a0
Jesse <jesse.morgan@hotmail.co.uk>
2022-05-23 12:31:00 +0100

    first

---
+++ src/main/java/com/qa/springbootExample/SpringbootExampleApplication.java
@@ -0,0 +1,22 @@
+package com.qa.springbootExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringbootExampleApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringbootExampleApplication.class, args);
+		
+		Object byName = context.getBean("hello");
+		String byType = context.getBean(String.class);
+		String byBoth = context.getBean("hello", String.class);
+		
+		System.out.println(byName);
+		System.out.println(byType);
+		System.out.println(byBoth);
+	}
+
+}
---
+++ src/main/java/com/qa/springbootExample/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.qa.springbootExample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public String hello() {
+		return "hello";
+	}
+
+}
